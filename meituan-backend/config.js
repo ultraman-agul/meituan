9848c279a1fb65fb229ab3a9bbbe35c52d034ab2
ultraman-agul <agulagul@163.com>
2021-12-25 18:09:33 +0800

    修改服务器地址

--- meituan-backend/config.js
+++ meituan-backend/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     port: 3000,         //启动端口
-    DB_URL: 'mongodb://127.0.0.1:27017/meituan',    //数据库地址
+    DB_URL: 'mongodb://1.14.132.215:27017/meituan',    //数据库地址
     notifyUrl: 'http://192.168.1.10:3000/v1/notify_url',      //支付异步通知地址
     // synNotifyUrl: 'http://39.108.3.12',              //客户端同步跳转
-    sessionStorageURL: 'mongodb://127.0.0.1:27017/session',   //数据库存放session地址
+    sessionStorageURL: 'mongodb://1.14.132.215:27017/session',   //数据库存放session地址
     Bucket: 'meituanaaa',   //七牛云bucket
     AccessKey: 'S7O0wCTHwaY8AhX8HPBgzrzdpoUzBLb5l8lDGawA',   //七牛云accessKey
     SecretKey: 'K5-OmF_ao-8NV3TqSCS7bLNf4tKeD-rA4DUY1UDt',    //七牛云secretKey
